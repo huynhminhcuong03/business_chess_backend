0fd9b7a0e78fb32816bef2a6fd60eccafb116990
huynhcuong270403 <huynhminhcuong>
2026-07-27 15:37:05 +0700

    Implement the board retrieval feature.

---
+++ src/main/java/com/businesschess/entities/Board.java
@@ -0,0 +1,36 @@
+package com.businesschess.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "boards")
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(
+            name = "created_at",
+            nullable = false,
+            insertable = false,
+            updatable = false
+    )
+    private LocalDateTime createdAt;
+}
